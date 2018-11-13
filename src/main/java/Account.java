955156e5ad45cf659eb4ffb1554ffd90dfd23b4c
xrs50 <gnanamany@hotmail.co.uk>
2018-11-13 11:12:45 +0000

    second commit in branch

---
+++ src/main/java/Account.java
@@ -0,0 +1,36 @@
+public class Account {
+    private String firstName;
+    private String lastName;
+    private int accountNumber;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Account(int accountNumber, String firstName, String lastName){
+        setAccountNumber(accountNumber);
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+}
--- src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Lets see if i'm here in the origin");
-    }
-}
